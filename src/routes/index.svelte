f61d2a9c6cd6a5925b53b9d4a3d113d0567e39ff
Oliver Read <oliver@reads.org.uk>
2021-05-13 13:21:49 +0100

    Set up tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,25 @@
+<script lang="ts">
+  import "../app.pcss";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<ul class="space-y-2">
+  <li>
+    <ul class="grid grid-cols-10 h-7">
+      <li class="bg-red-50" />
+      <li class="bg-red-100" />
+      <li class="bg-red-200" />
+      <li class="bg-red-300" />
+      <li class="bg-red-400" />
+      <li class="bg-red-500" />
+      <li class="bg-red-600" />
+      <li class="bg-red-700" />
+      <li class="bg-red-800" />
+      <li class="bg-red-900" />
+    </ul>
+  </li>
+</ul>
